93a2d7741ddea2f1aeb3d3d2d4f634780396c2ca
test <test>
2022-11-07 19:27:12 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,11 @@ export default function Home() {
       </div>
 
       <main className={styles.main}>
+	<a href="/ipv4.html">Ip Rechner</a>
         <svg height="100%" width="100%" className={styles.svg}>
           <clipPath id="text-overlay">
             <text x="50%" y="50%" fill="red" textAnchor="middle">
-              Bobby68
+              Die IT Profis
             </text>
           </clipPath>
         </svg>
