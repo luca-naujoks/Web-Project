b58ee982853ee198faa19fa1691d804ee05419d4
Bobby68 <88344957+luca-naujoks@users.noreply.github.com>
2022-11-20 21:09:29 +0100

    Add files via upload
    Complete update to Tailwindcss 
    without index svg clip

---
+++ pages/to-do.jsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+      <a className="font-bold">halloo</a>
+    )
+  }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/*.{html,js,jsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      olors: {
+        'thin-blue': '#161b22',
+      },
+    },
   },
   plugins: [],
 }
