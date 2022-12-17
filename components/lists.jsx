640394aba9f3aaca0868a62db9de9f77108f6ca2
Luca Naujoks <luca@bobby68.de>
2022-12-17 11:23:52 +0100

    push for pc

--- components/lists.jsx
+++ components/lists.jsx
@@ -2,48 +2,38 @@ import { useEffect, useState } from "react";
 import React from "react";
 
 export function Lists() {
-  const todos = [
-    "Einkaufen gehen",
-    "Zähne putzen",
-    "Küche aufräumen",
-    "Bügeln",
-    "Abendessen kochen",
-    "Rechnungen bezahlen",
-    "E-Mails checken",
-    "Fenster putzen",
-    "Staubsaugen",
-    "Blumen gießen",
-    "Zimmerschränke aufräumen",
-    "Rasen mähen",
-    "Auto waschen",
-    "Garagenverkauf organisieren",
-    "Briefe schreiben",
-    "Fotos organisieren",
-    "Kontoauszüge ausdrucken",
-    "Geschenke kaufen",
-    "Bücher zurückbringen",
-    "Dokumente ablegen",
-    "Handy aufladen",
-    "Schuhe putzen",
-    "Nägel schneiden",
-    "Kleidung ausrangieren",
-    "Schminkutensilien auffüllen",
-    "Hund ausführen",
-    "Klavier üben",
-    "Museum besuchen",
-    "Wandmalerei anfertigen",
-    "Schwimmen gehen",
-    "Yoga machen",
-    "Ausflug planen",
-    "Bilderrahmen aufhängen",
-    "Briefkasten leeren",
-    "Kühlschrank aufräumen",
-    "Geschirr spülen",
-    "Abendessen vorbereiten",
-    "Gartenpflanzen pflegen",
-    "Reise buchen",
-    "Treffen mit Freunden organisieren",
-  ];
+  const todos = localStorage.getItem("todo").split(",");
+
+  function addtodo() {
+    let oldtodos = localStorage.getItem("todo");
+    let input = document.getElementById("todo-input");
+    let inputvalue = oldtodos + "," + input.value;
+    localStorage.setItem("todo", inputvalue);
+
+    let ul = document.querySelector("#todolist");
+    let li = document.createElement("li");
+
+    let id = input.value
+
+    li.textContent = input.value;
+    li.setAttribute("id", input.value);
+    console.log(input.value)
+    li.setAttribute('onclick', function checklist() {
+      let li_item = input.value
+      document.getElementById(li_item).classList.add("line-through");
+    })
+    li.setAttribute(
+      "class",
+      "bg-[#161b22] text-xl rounded-md h-14 m-2 p-2 cursor-pointer"
+    );
+
+
+    ul.appendChild(li);
+  }
+
+  function checkli(id) {
+    document.getElementById(id).classList.add("line-through");
+  }
 
   return (
     <div id="main" className="overflow-hidden">
@@ -52,21 +42,32 @@ export function Lists() {
       </div>
       <div>
         <div id="list" className="flex text-white">
-          <ul className="w-[100%] overflow-y-hidden hover:overflow-y-scroll scroll-smooth h-200">
+          <ul
+            id="todolist"
+            className="w-[100%] overflow-y-hidden hover:overflow-y-scroll scroll-smooth h-200"
+          >
             {todos.map((item) => (
               <li
                 id={item}
                 key={item}
-                className="bg-[#161b22] text-xl rounded-md h-14 m-2 p-2 cursor-default"
+                className="bg-[#161b22] text-xl rounded-md h-14 m-2 p-2 cursor-pointer"
+                onClick={function check() {
+                  document.getElementById(item).classList.add("line-through");
+                }}
               >
-                <span className="hover:text-green-700 cursor-pointer pr-3">&#10004;</span>
-                {item}
+                <a>{item}</a>
               </li>
             ))}
           </ul>
         </div>
-        <div className="bg-[#161b22] h-14 w-auto m-2 mt-6 p-2 rounded-md justify-center">
-          Add New Task +
+        <div className="flex bg-[#161b22] h-14 w-auto m-2 mt-6 p-2 rounded-md items-center justify-center">
+          <input
+            id="todo-input"
+            type="search"
+            placeholder=" Add New Task"
+            className="flex bg-transparent text-xl focus:border-none focus:rounded-none"
+          />
+          <button onClick={addtodo}>Add Task</button>
         </div>
       </div>
     </div>
