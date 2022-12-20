4d9b9064d972b349aaefc4168761beedca389636
luca-naujoks <lucapatricknaujoks@googlemail.com>
2022-12-20 14:43:43 +0100

    10

--- pages/about_me.jsx
+++ pages/about_me.jsx
@@ -27,7 +27,7 @@ export default function about_me() {
           <img
             src="/assets/about_me/header_image.png"
             alt="Bobby68"
-            className="xs:w-2/3 md:w-1/2"
+            className="md:hidden"
           />
           <Auth />
         </div>
